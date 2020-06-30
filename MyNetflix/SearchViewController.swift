798fe080d8c181831b97c6959b36f940352abf2d
joe <homo.technologicus1@gmail.com>
2020-06-30 19:01:48 +0900

    initial UI layout and connect a search bar to code

---
+++ MyNetflix/SearchViewController.swift
@@ -0,0 +1,41 @@
+//
+//  SearchViewController.swift
+//  MyNetflix
+//
+//  Created by joonwon lee on 2020/04/02.
+//  Copyright © 2020 com.joonwon. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var resultCollectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+extension SearchViewController: UISearchBarDelegate {
+    
+    private func dismissKeyboard() {
+        searchBar.resignFirstResponder()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // send keyboard down when it is up
+        dismissKeyboard()
+        
+        // ensure if there exists a search word
+        guard let searchTerm = searchBar.text, searchTerm.isEmpty == false else { return }
+        
+        
+        // search through networking
+        
+        
+        print("--> search word: \(searchBar.text)")
+    }
+}
