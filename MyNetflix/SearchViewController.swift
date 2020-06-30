219f68618adf57ce02be8f2df5165c457ec950f0
joe <homo.technologicus1@gmail.com>
2020-07-01 02:55:55 +0900

    render images in SearchViewController using Kingfisher library

--- MyNetflix/SearchViewController.swift
+++ MyNetflix/SearchViewController.swift
@@ -7,15 +7,58 @@
 //
 
 import UIKit
+import Kingfisher
 
 class SearchViewController: UIViewController {
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var resultCollectionView: UICollectionView!
     
+    var movies: [Movie] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+}
+
+extension SearchViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ResultCell", for: indexPath) as? ResultCell else {
+            return UICollectionViewCell()
+            
+        }
+        
+        let movie = movies[indexPath.item]
+        let url = URL(string: movie.thumbnailPath)!
+        cell.movieThumbnail.kf.setImage(with: url)
+        
+        return cell
+    }
+    
+    
+}
+
+extension SearchViewController: UICollectionViewDelegate {
+    
+}
+
+extension SearchViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let margin: CGFloat = 8
+        let itemSpacing: CGFloat = 10
+        let width = (collectionView.bounds.width - margin * 2 - itemSpacing * 2) / 3
+        let height = width * 10 / 7
+        return CGSize(width: width, height: height)
+    }
+}
+
+class ResultCell: UICollectionViewCell {
+    @IBOutlet weak var movieThumbnail: UIImageView!
     
 }
 
@@ -35,13 +78,17 @@ extension SearchViewController: UISearchBarDelegate {
         
         // search through networking
         // - aim: search movies with a search term
-        // - search API required
-        // - data model: Movie, Response
-        // - get the result, and render CollectionView
+        // - [x] search API required
+        // - [x] data model: Movie, Response
+        // - [x] get the result, and render CollectionView
         
         SearchAPI.search(searchTerm) { movies in
-            // render collectionView
+            // [x] render collectionView
             print("--> How many movies?? \(movies.count), the first one's title: \(movies.first?.title)")
+            DispatchQueue.main.async {
+                self.movies = movies
+                self.resultCollectionView.reloadData()
+            }
         }
         
         
